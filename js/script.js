7bb2aa9adf5f71e77e06bb4499e6dc1bb699dcf5
komiljonov-husanboy <142677088+komiljonov-husanboy@users.noreply.github.com>
2023-09-29 12:20:27 +0500

    hisob

--- js/script.js
+++ js/script.js
@@ -7,15 +7,20 @@ var elBtn = document.querySelector('.Btn');
 var a = 500 * 11000;
 var b = 250 * 11000;
 var c = 120 * 13000;
-
 var v = a + b + c;
+var remainingAmount = v - elNumber.value;
+// var d = v - elNumber.value
 elTitle.textContent = "sayohat uchun yetarli mablag' :    " + v + " so'm";
 
 elBtn.addEventListener('click', function() {
     if (elNumber.value > v || elNumber.value == v) {
-        elTitle.textContent = "Tabriklayman sayohat uchun mablag' yetdi, sizning pulingiz : " + elNumber.value + " so'm";
-        elBtn.disabled = true; // Tekshirish tugmasini o'chirish
+        elTitle.textContent = "Tabriklaymiz, sayohat uchun yetarli mablag'ingiz bor! Sizning pulingiz: " + elNumber.value + " so'm";
+        elBtn.disabled = true;
     } else {
-        elTitle.textContent = "Afsuski sayohat uchun mablag' yetmaydi, sizning pulingiz : " + elNumber.value + " so'm";
+        elTitle.textContent = "Afsuski, sayohat uchun mablag' yetarli emas. Sizning pulingiz: " + elNumber.value + " so'm     ,";
+        var qoldi = v - elNumber.value;
+        if (qoldi > 0) {
+            elTitle.textContent += "  Sizga yana : " + qoldi + " so'm   "+"yetmadi";
+        }
     }
 });
